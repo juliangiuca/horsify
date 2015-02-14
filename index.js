a76a08e7f49a3196c2592c4443728066e6eec814
Julian Giuca <julian.giuca@gmail.com>
2015-02-14 14:49:25 -0800

    cant add the hose if the face is too close to the edge

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ var images = require("images");
 var Q = require('q');
 
 
-//cv.readImage("./examples/files/mona.png", function(err, im){
-//  im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
-//
-//    //for (var i=0;i<faces.length; i++){
-//    //  var x = faces[i]
-//    //  im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
-//    //}
-//    //im.save('./out.jpg');
-//
-//  });
-//})
+var circle = function (faceSrc) {
+  cv.readImage("./cory.jpg", function(err, im){
+    im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
+
+      for (var i=0;i<faces.length; i++){
+        var x = faces[i]
+        im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+      }
+      im.save('./out.jpg');
+
+    });
+  })
+};
 
 
 var cvReadImage = function (img) {
@@ -44,17 +46,34 @@ var detectFace = function (im) {
   return deferred.promise;
 }
 
-var face = "./examples/files/mona.png";
-var horse = "./horse.jpg";
+var faceSrc = "./erica.jpg";
+var horseSrc = "./horse.png";
 
-cvReadImage(face)
-.then(detectFace)
-.then(function (faces) {
-  var x = faces[0];
+var horsify = function (faceSrc) {
+  cvReadImage(faceSrc)
+  .then(detectFace)
+  .then(function (faces) {
+    for (var i=0; i<faces.length; i++) {
+      var face = faces[i];
+      var horse = images(horseSrc);
+      var max = (face.width > face.height ? face.width : face.height);
+      horse.size(max*2.5);
 
-  images(face)
-  .draw(images(horse), x.width/2, x.height/2)
-  .save('foo.jpg');
-}).fail(function (err) {
-  console.log(err);
-})
+      var newX = face.x - (horse.width()/2.7);
+      var newY = face.y - (horse.height()/4);
+
+      console.log(newX, newY);
+      newX = newX > 0 ? newX : 0;
+      newY = newY > 0 ? newY : 0;
+
+      images(faceSrc)
+      .draw(horse, newX, newY)
+      .save('foo.jpg');
+
+    }
+  }).fail(function (err) {
+    console.log(err);
+  })
+}
+
+horsify(process.argv[2]);
