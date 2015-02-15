651dd4e935691de7ca8dd425f587f49118acdca7
Julian Giuca <julian.giuca@gmail.com>
2015-02-14 22:08:50 -0800

    works left and right

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var cv = require('opencv');
 var images = require("images");
 var Q = require('q');
+var gm = require('gm');
+var fs = require('fs');
+var im = require('imagemagick');
 
 
 var circle = function (faceSrc) {
-  cv.readImage("./cory.jpg", function(err, im){
+  cv.readImage(faceSrc, function(err, im){
     im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
 
       for (var i=0;i<faces.length; i++){
@@ -46,34 +49,101 @@ var detectFace = function (im) {
   return deferred.promise;
 }
 
-var faceSrc = "./erica.jpg";
-var horseSrc = "./horse.png";
+var fetchSize = function (horse) {
+  var deferred = Q.defer();
 
-var horsify = function (faceSrc) {
+  horse.size(function (err, size) {
+    if (err) {
+      deferred.reject(err);
+    } else {
+      deferred.resolve(size);
+    }
+  });
+
+  return deferred.promise;
+}
+
+//var horseSrc = (Math.random() > 0.5 ? './horse.png' : './horseFlip.png');
+var horseSrc = './horse.png';
+
+var horseFace = function (orient) {
+  var img = {}
+
+  if (!orient) {
+    img.src = './horseFlip.png';
+    img.orient = 'right';
+  } else if (orient == 'left') {
+    img.src = './horse.png';
+    img.orient = 'left';
+  } else {
+    img.src = './horseFlip.png';
+    img.orient = 'right';
+  }
+
+  return img;
+}
+
+var rightSkew = { x: 4, y: 2.5 };
+var leftSkew = { x: 2.2, y: 3.3 };
+
+var horsify = function (faceSrc, orientation) {
+  var horse = horseFace(orientation);
+
   cvReadImage(faceSrc)
   .then(detectFace)
   .then(function (faces) {
     for (var i=0; i<faces.length; i++) {
+
       var face = faces[i];
-      var horse = images(horseSrc);
-      var max = (face.width > face.height ? face.width : face.height);
-      horse.size(max*2.5);
+      var horseGm = gm(horse.src);
+      var sourceGm = gm(faceSrc);
 
-      var newX = face.x - (horse.width()/2.7);
-      var newY = face.y - (horse.height()/4);
+      Q.all([fetchSize(horseGm),fetchSize(sourceGm)])
+      .then(function (sizes) {
 
-      console.log(newX, newY);
-      newX = newX > 0 ? newX : 0;
-      newY = newY > 0 ? newY : 0;
+        var horseSize       = {width: sizes[0].width, height: sizes[0].height};
+        var originalPicSize = {width: sizes[1].width, height: sizes[1].height};
+        var faceSize        = {width: face.width, height: face.height};
 
-      images(faceSrc)
-      .draw(horse, newX, newY)
-      .save('foo.jpg');
+        // Make the horse 2.3 times larger than the person's face
+        newHorse = { height: Math.round(faceSize.height * 2.3) };
+
+        console.log('orient', horse.orient);
+        if (horse.orient === 'left') {
+          x = leftSkew.x;
+          y = leftSkew.y;
+        } else {
+          x = rightSkew.x;
+          y = rightSkew.y;
+        }
+
+        newHorse.ratio = (horseSize.width / horseSize.height);
+        newHorse.width = Math.round(newHorse.height * newHorse.ratio);
+        newHorse.x     = Math.round(face.x - (newHorse.width / x));
+        newHorse.y     = Math.round(face.y - (newHorse.height / y));
+
+        var position = newHorse.width + 'x' + newHorse.height;
+        position += (newHorse.x >= 0 ? '+' : '');
+        position += newHorse.x;
+        position += (newHorse.y >= 0 ? '+' : '');
+        position += newHorse.y;
+        console.log(position);
+
+        im.convert(['-composite', faceSrc, horse.src, '-geometry', position, 'foo.jpg'], function (err, stdout) {
+          console.log('err', err);
+          console.log('stdout', stdout);
+        });
+
+      }).fail(function (err) {
+        console.log('size err:', err);
+      });
 
     }
   }).fail(function (err) {
     console.log(err);
+    console.log(err.stack);
   })
 }
 
-horsify(process.argv[2]);
+horsify(process.argv[2], process.argv[3]);
+circle(process.argv[2]);
