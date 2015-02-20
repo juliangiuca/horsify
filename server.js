608d10d9dcb871ad80b31af366b94d81050048d1
Julian Giuca <julian.giuca@gmail.com>
2015-02-20 09:06:17 -0800

    buffer, streams, gm, and gyou

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+var express = require('express');
+var app = express();
+var horse = require('./lib/imageHandler.js');
+var fs = require('fs');
+
+app.set('view engine', 'jade');
+
+app.get('/horsify', function (req, res){
+  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  horse.byUrl(req.query.url, req.query.o)
+  .then(function (gm) {
+    gm.stream('jpg', function (err, stdout, stderr) {
+      console.log('err:', err);
+      console.log('stderr:', stderr);
+      stdout.pipe(res);
+    })
+  })
+});
+
+app.get('/', function (req, res){
+
+  //console.log(req.query.url);
+  res.send('hello world');
+});
+
+app.post('/', function (req, res) {
+  var form = new formidable.IncomingForm();
+  form.uploadDir = "./tmp";
+  form.keepExtensions = true;
+
+  //horse.byImage('./chris.jpg', req.query.o)
+
+  form.on('end', function() {
+  });
+});
+
+app.listen(3000);
+
