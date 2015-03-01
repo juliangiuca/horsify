cdd42d4c3255b268b7c8fafc6cfe0e8771209cb0
Julian Giuca <julian.giuca@gmail.com>
2015-03-01 15:13:56 -0800

    caching and retries!

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 'use strict';
 require('newrelic');
-
 var express     = require('express');
 var app         = express();
 var log         = require('./lib/logging.js');
 var imageLoader = require('./lib/imageLoader.js');
+var retryableStream = require('./lib/retryableStream.js');
+var _ = require('lodash');
 
 app.set('view engine', 'jade');
 app.use(require('express-bunyan-logger')());
@@ -18,40 +19,26 @@ app.get('/url', function (req, res){
 
 });
 
+
 // Show an image
 app.get('/:sha', function (req, res) {
   var sha = req.params.sha;
-  var stream = imageLoader.findBySha(sha);
-
-  stream.on('error', function (error) {
-    log.error('Caught', error);
-    setTimeout(function () {
-      res.redirect('/' + sha);
-    }, 1000);
-  });
-
-  //res.writeHead(200, {'Content-Type': 'image/jpg' });
-  stream.pipe(res);
+  retryableStream(imageLoader.findBySha(sha), res, 0);
 });
 
 app.get('/', function (req, res) {
+  var images = [
+    {url: 'http://www.wallpaperfo.com/thumbnails/detail/20120429/ellen%20page%202400x1800%20wallpaper_www.wallpaperfo.com_11.jpg', o: 'right'},
+    {url: 'http://resources3.news.com.au/images/2009/11/30/1225805/212259-tony-abbott-091130.jpg', o: 'right'},
+    {url: 'http://upload.wikimedia.org/wikipedia/commons/8/8d/President_Barack_Obama.jpg', o: 'left'}
+  ]
 
-  //console.log(req.query.url);
-  res.send('hello world');
+  var pic = images[_.random(0, images.length-1)];
+  res.redirect('/url?url=' + pic.url + '&o=' + pic.o );
 });
 
-//app.post('/', function (req, res) {
-//  var form = new formidable.IncomingForm();
-//  form.uploadDir = "./tmp";
-//  form.keepExtensions = true;
-//
-//  //horse.byImage('./chris.jpg', req.query.o)
-//
-//  form.on('end', function() {
-//  });
-//});
 
 app.listen(3000, function () {
-  log.info('server up');
+  log.info('server up - log');
 });
 
